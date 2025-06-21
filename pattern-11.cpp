1b55d823417399b4a0a9096193390573a920b23c
alpana singh <alpanasinghcs@gmail.com>
2025-06-21 10:43:07 +0530

    pattern using for loop

---
+++ pattern-11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=4; i>=1; i--){
+        for(int j=i+1; j<=4; j++){
+            cout <<" * "; 
+    }
+    cout << endl;
+    }
+return 0;
+}
