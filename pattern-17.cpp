ff879fa967203a86eb590364fe0c80547a41f986
alpana singh <alpanasinghcs@gmail.com>
2025-06-23 22:24:43 +0530

    Added pattern printing for multiple symbols based on row number

--- pattern-17.cpp
+++ pattern-17.cpp
@@ -3,11 +3,15 @@ using namespace std;
 int main(){
    for(int i=1; i<=5; i++){
     for(int j=1; j<=5; j++){
-        if(i==1 || i==2 || i==4 || i==5){
+         if(i==1 || i==5){
             cout << "1";
         }else{
-            if( i==3 ){
+            if(i==2){
                 cout <<  "*"  ;
+            }else if(i==3){
+                    cout << "$" ;
+                }else if(i==4){
+                        cout << "@" ;
              }
         }
     }
