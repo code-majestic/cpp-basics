81f3c4b62bac2a51f91b9a3638baf3c2a3dc6fcf
alpana singh <alpanasinghcs@gmail.com>
2025-06-26 14:03:14 +0530

    print solid half diamond

--- pattern-24.cpp
+++ pattern-24.cpp
@@ -1,36 +1,17 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-    for (int i = 6; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << " ";
+int main(){
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
         }
-        for (int k = 1; k <= 7 - i; k++)
-        {
-            if(k==1 || (k==7-i)
-              cout <<"* ";
-            else
-              cout<<"  ";
-        }
-        cout << endl;
+        cout<<endl;
     }
-    for (int i = 1; i <= 6; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "  ";
+    for(int i=4; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
         }
-        for (int k = 1; k <= 7 - i; k++)
-        {
-            if(k==1 || (k==7-i)
-            cout <<"* ";
-            else
-            cout<<"  ";
-        }
-        cout << endl;
-    }
-    return 0;
+        cout<<endl;
+}
+return 0;
 }
