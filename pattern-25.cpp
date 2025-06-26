8b16a256f021501a61a24eaf3fcd771e83b58bcd
alpana singh <alpanasinghcs@gmail.com>
2025-06-26 14:20:19 +0530

    print number pyramid

---
+++ pattern-25.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=3; i<=6; i++){
+        for(int j=3; j<=i; j++){
+            cout<< i<<" ";
+        }
+        cout<<endl;
+    }
+   for(int i=5; i>=3; i--){
+        for(int j=3; j<=i; j++){
+            cout<< i<<" ";
+        }
+        cout<<endl; 
+}
+return 0;
+}
