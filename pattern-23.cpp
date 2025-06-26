2a96efa4b24c2bab2f123c0279bb47667756f316
alpana singh <alpanasinghcs@gmail.com>
2025-06-26 13:53:09 +0530

    print hollo diamond

--- pattern-23.cpp
+++ pattern-23.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    for (int i = 6; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-        {
+int main(){
+    for(int i = 6; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) {
             cout << " ";
         }
-        for (int k = 1; k <= 7 - i; k++)
-        {
-            if (k == 1 || (k+i)==7)
-                cout << "* ";
-            else
-                cout << "   ";
-        }
-        cout << endl;
-    }
-    for (int i = 1; i <= 6; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "  ";
-        }
-        for (int k = 1; k <= 7 - i; k++)
-        {
-            if (k == 1 || (k+i)==7)
-                cout << "* ";
-            else
-                cout << "   ";
-        }
-        cout << endl;
-    }
 
+        int k;
+        for(k = 1; k <= 7; k++) {
+            if(k == 1 || (k+i)==7) 
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+
+        cout << endl;
+    }
+    for(int i = 1; i <= 6; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << " ";
+        }
+
+        int k;
+        for(k = 1; k <= 7; k++) {
+            if(k == 1 || (k+i)==7) 
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+
+        cout << endl;
+    }
     return 0;
 }
