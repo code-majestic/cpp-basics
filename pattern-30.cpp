824381aeb713dbb683eeec3c22b2d6e44f537174
alpana singh <alpanasinghcs@gmail.com>
2025-06-28 09:17:54 +0530

    Palidromic Number Pyramid with Cetere Alignment

--- pattern-30.cpp
+++ pattern-30.cpp
@@ -2,21 +2,14 @@
 using namespace std;
 int main()
 {
-    int i, j, s;
-    for (i = 1; i <= 4; i++)
+    for (int i = 1; i <= 4; i++)
     {
-        for (s = i; s <= 4; s++) 
+        for (int s = 1; s <= 4 - i; s++)
             cout << " ";
-        for (j = 1; j <= i; j++)
-            cout << "* ";
-        cout << endl;
-    }
-    for (i = 3; i >= 1; i--)
-    {
-        for (s = 4; s >= i; s--) 
-            cout << " ";
-        for (j = 1; j <= i; j++)
-            cout << "* ";
+        for (int j = 1; j <= i; j++)
+            cout << j << " ";
+        for (int j = i - 1; j >= 1; j--)
+            cout << j << " ";
         cout << endl;
     }
 }
