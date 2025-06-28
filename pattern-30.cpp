74785ec47ae6fba98d7369a4d0a1c895fd0a9210
alpana singh <alpanasinghcs@gmail.com>
2025-06-28 09:20:18 +0530

    X star pattern (cross)

--- pattern-30.cpp
+++ pattern-30.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i <= 4; i++)
+    int n = 5;
+    for (int i = 1; i <= n * 2 - 1; i++)
     {
-        for (int s = 1; s <= 4 - i; s++)
-            cout << " ";
-        for (int j = 1; j <= i; j++)
-            cout << j << " ";
-        for (int j = i - 1; j >= 1; j--)
-            cout << j << " ";
+        for (int j = 1; j <= n * 2 - 1; j++)
+        {
+            if (j == i || j == (n * 2 - i))
+                cout << "*";
+            else
+                cout << " ";
+        }
         cout << endl;
     }
 }
